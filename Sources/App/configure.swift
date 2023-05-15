a86114f5c0b870caf36d5d6fa9604966e586f60e
Sara-Kuddah <sara-kuddah@hotmail.com>
2023-05-15 09:19:26 +0300

    first commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,9 +10,9 @@ public func configure(_ app: Application) async throws {
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        username: Environment.get("DATABASE_USERNAME") ?? "sarabinkuddah",
+        password: Environment.get("DATABASE_PASSWORD") ?? "",
+        database: Environment.get("DATABASE_NAME") ?? "split"
     ), as: .psql)
 
     app.migrations.add(CreateTodo())
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,14 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
-
+    
+    app.get("hello", "Sara") { req in
+        return "Hello, Sara!"
+    }
+    
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)!"
+    }
     try app.register(collection: TodoController())
 }
